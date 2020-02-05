195df4b0ae5d2d49b6109147bce41e2fe83f8b9e
Anthony <anthony.lahlah.partner@decathlon.com>
2020-02-05 17:47:42 +0100

    fix: change status bar color without plugin and set navigation bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:cvmakr/screens/home.dart';
-import 'package:cvmakr/screens/personal_informations.dart';
 import 'package:provider/provider.dart';
 import 'package:cvmakr/data/data.dart';
-
+import 'package:flutter/services.dart';
 import 'screens/home.dart';
 
 Color primaryColor = Color(0xFF8B84FB);
-//const String sharedKey = 'personal_infos';
 const String sharedKey = 'data';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: primaryColor, // status bar color
+    systemNavigationBarColor: Colors.white, // navigation bar color
+  ));
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    FlutterStatusbarcolor.setStatusBarColor(primaryColor);
     return FutureBuilder(
         future: Data.restoreData(),
         builder: (context, AsyncSnapshot<Data> snapshot) {
@@ -25,7 +28,7 @@ class MyApp extends StatelessWidget {
             return Provider(
               create: (_) => snapshot.data,
               child: MaterialApp(
-                  title: 'Flutter Demo',
+                  title: 'cvMakR',
                   theme: ThemeData(
                       fontFamily: 'Metropolis', primaryColor: primaryColor),
                   home: HomePage()),
