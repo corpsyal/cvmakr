2e7adaeafddcce6b23823887283b01ce2de9d83f
Anthony <lahlah.anthony@gmail.com>
2020-03-03 22:38:21 +0100

    add splashscreen (programatically)

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,21 @@
-import 'package:cvmakr/consts.dart';
 import 'package:cvmakr/data/data.dart';
 import 'package:cvmakr/screens/home.dart';
 import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' as Intl;
 import 'package:provider/provider.dart';
-
-import 'screens/home.dart';
 
 Color primaryColor = Color(0xFF8B84FB);
 const String sharedKey = 'data';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized(); // need when async in main
-  Intl.defaultLocale = 'fr';
+  Intl.Intl.defaultLocale = 'fr';
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
     statusBarColor: primaryColor, // status bar color
-    systemNavigationBarColor: Colors.white, // navigation bar color
+    systemNavigationBarColor: primaryColor, // navigation bar color
   ));
 
   FirebaseAdMob.instance
@@ -31,21 +28,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    RewardedVideoAd.instance
-        .load(
-          adUnitId: RewardedVideoAd.testAdUnitId,
-          //adUnitId: 'ca-app-pub-8039402823283760/1332869913',
-          targetingInfo: targetingInfo,
-        )
-        .catchError((e) => print("error in loading 1st time"))
-        .then((v) => print('result ici : $v'));
-
     return FutureBuilder(
         future: Data.restoreData(),
         builder: (context, AsyncSnapshot<Data> snapshot) {
-          print(snapshot.data);
           if (snapshot.connectionState == ConnectionState.done) {
             print(snapshot.data.experiences.length);
+            SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+              systemNavigationBarColor: Colors.white, // navigation bar color
+            ));
+
             return ChangeNotifierProvider(
               create: (_) => snapshot.data,
               child: MaterialApp(
@@ -55,7 +46,27 @@ class MyApp extends StatelessWidget {
                   home: HomePage()),
             );
           } else {
-            return Container();
+            return Container(
+              color: primaryColor,
+              child: Center(
+                child: RichText(
+                  textDirection: TextDirection.ltr,
+                  text: TextSpan(
+                    text: 'cv',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontFamily: 'Metropolis',
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Makr',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+              ),
+            );
           }
         });
   }
